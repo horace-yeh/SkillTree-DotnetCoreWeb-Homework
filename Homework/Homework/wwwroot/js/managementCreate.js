aaec259210373b2d0e2cf6528c06eccae38a3250
horace.yeh <horaceyeh.web@gmail.com>
2021-01-02 18:49:47 +0800

    新增文章(未完成) 1.日期增加 datepick套件，取都用cdn

--- Homework/Homework/wwwroot/js/managementCreate.js
+++ Homework/Homework/wwwroot/js/managementCreate.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-
     function inputFileInit() {
         $('#CoverPhotoImg').on('change', function () {
             //get the file name
@@ -7,9 +6,19 @@
             //replace the "Choose a file" label
             $(this).next('.custom-file-label').html(fileName);
         })
-    } 
+    }
 
+    function datePickerInit() {
+        // ref: https://amsul.ca/pickadate.js/date/
+        $('#CreateDate').pickadate({
+            showMonthsShort: true,
+            format: 'yyyy/mm/dd',
+            formatSubmit: 'yyyy-mm-dd',
+            hiddenName: true
+        })
+    }
 
     // call init
     inputFileInit();
+    datePickerInit();
 });
